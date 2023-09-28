a415cf9457c587891d01e31933dd640748814d7c
minji-yun <minpaper.dev@gmail.com>
2023-09-28 19:51:51 +0900

    [fix] 사용하지 않는 import 삭제

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import ColorItem from '../components/ColorItem'
 import styled from 'styled-components'
 import logo from '../assets/logo.png'
