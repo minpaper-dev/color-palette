d09b953eb60ecbb2e553c98e413f9ec430a76534
minji-yun <minpaper.dev@gmail.com>
2023-09-29 21:19:34 +0900

    [Feat] 유효한 색상일 경우 검색 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
 import logo from '../assets/logo.png'
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import tinycolor from 'tinycolor2'
 
 const Logo = styled.img`
   width: 30%;
@@ -15,13 +18,33 @@ const Input = styled.input`
   margin-left: 2rem;
 `
 
+const Button = styled.button`
+  margin-left: 1rem;
+  padding: 0 1rem;
+`
+
 const Header = () => {
+  const navigate = useNavigate()
+
+  const [inputValue, setInputValue] = useState('')
+
+  const navigateTo = () => {
+    const color = tinycolor(inputValue).isValid()
+    if (tinycolor(inputValue).isValid()) {
+      const noHash = inputValue.replace('#', '')
+      navigate(`/color/${noHash}`)
+    }
+  }
   return (
     <>
       <Logo src={logo} />
       <WrapSearch>
         Search Your Color !
-        <Input />
+        <Input
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <Button onClick={navigateTo}>🔍</Button>
       </WrapSearch>
     </>
   )
