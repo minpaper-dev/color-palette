728993255da70e5cd19092a604b99bbc9debb13c
mlnpaper <minpaper.dev@gmail.com>
2023-12-08 00:48:15 +0900

    [data] 주요 색상 정의

---
+++ src/data/major.ts
@@ -0,0 +1,32 @@
+export const MAJOR_COLOR = [
+  {
+    color: 'FF6C72',
+  },
+  {
+    color: 'FFB3B6',
+  },
+  {
+    color: 'FFA37A',
+  },
+  {
+    color: 'FFDA72',
+  },
+  {
+    color: '1FCF7D',
+  },
+  {
+    color: '3EA0E0',
+  },
+  {
+    color: '9B91C6',
+  },
+  {
+    color: '9C6668',
+  },
+  {
+    color: 'C0BEBF',
+  },
+  {
+    color: '4A4A4A',
+  },
+]
